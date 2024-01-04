dc9ce87b0423ca361920a7d6d349ba0b8c75b46f
jasonbanboa <104666296+jasonbanboa@users.noreply.github.com>
2024-01-04 05:06:50 +0000

    changed http to https

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 
 async function main() {
-    const { result } = await fetch('http://3.36.34.86:1234/add?a=12&b=34').then(res => res.json());
+    const { result } = await fetch('https://3.36.34.86:1234/add?a=12&b=34').then(res => res.json());
     document.body.append(result);
 }
 main();
